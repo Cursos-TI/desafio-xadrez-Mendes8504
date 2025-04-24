4477d710451a902752fe5c758991c792dc7e47c4
Mendes8504 <mendess.8677@gmail.com>
2025-04-24 14:50:17 +0000

    Implentando os movimentos do cavalo

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,7 @@
 int main() {
     
         //Declarando e iniciando as variaveis com valor 0
-    int t = 0, b = 0, r = 0;
+    int t = 0, b = 0, r = 0, c = 0;
 
 
         //Laço de repetição simulando a movimentação da Torre
@@ -37,6 +37,23 @@ int main() {
         printf("Esquerda\n");
         r++;
     } while (r <= 7);
+    
+
+            printf("--------------\n");
+
+
+         //Laço de repetição simulando a movimentação do Cavalo
+    printf("Cavalo se moveu para:\n");
+    for (c; c < 1; c++)
+    {
+        while (c < 2)
+        {
+            printf("Baixo\n");
+            c++;
+        }
+        
+        printf("Esquerda\n");
+    }
     
     
     return 0;

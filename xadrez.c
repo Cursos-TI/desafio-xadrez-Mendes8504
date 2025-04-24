a6158ebc6d75a1f74d31a2be871d4379e9976dba
Mendes8504 <mendess.8677@gmail.com>
2025-04-24 14:13:07 +0000

    Movimentação das peças

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,43 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    
+        //Declarando e iniciando as variaveis com valor 0
+    int t = 0, b = 0, r = 0;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+        //Laço de repetição simulando a movimentação da Torre
+    printf("Torre moveu-se para:\n");
+    for (t ; t < 5; t++)
+    {
+    printf("Direita\n");
+    }
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+        printf("--------------\n");
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+        //Laço de repetição simulando a movimentação do Bisbo
+    printf("Bispo moveu-se para:\n");
+    while (b < 5)
+    {
+        printf("Cima, Direita\n");
+        b++;
+    }
 
+
+        printf("--------------\n");
+
+
+         //Laço de repetição simulando a movimentação da Rainha
+    printf("Rainha se moveu para:\n");
+    do
+    {
+        printf("Esquerda\n");
+        r++;
+    } while (r <= 7);
+    
+    
     return 0;
 }
